13cba738c3911a356d08be0938b5f7b8b10b2762
erwingm <erwin.guerra.mamani@gmail.com>
2018-04-29 20:00:54 -0500

    factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Project::class, function (Faker $faker) {
+    return [
+        //
+        'owner' => $faker->name,
+        'assigned_by' => $faker->name,
+        'name' => $faker->company,
+        'cost' => $faker->randomNumber(6),
+        'description' => $faker->sentence(100)
+    ];
+});
